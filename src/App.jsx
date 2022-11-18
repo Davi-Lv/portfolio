796ee453c195cffd900a8842d723f50b24a82b51
Davi-Lv <josedavimordonhoguimaraes@gmail.com>
2022-11-17 21:44:07 -0300

    iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import styles from './App.css'
+
+export default function App() {
+  return (
+    <body className="App">
+
+      <div className='menu'>
+        <div className="nav">
+          <div className='buttomNav'>
+            Sobre min
+          </div>
+          <div className='buttomNav'>
+            GitHub
+          </div>
+          <div className='buttomNav'>
+            Meus projetos
+          </div>
+        </div>
+
+
+        <div className='botaoContatos'>
+          Meus Contatos
+        </div>
+      </div>
+
+    </body>
+  )
+}
+
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function Home() {
+  return (
+    <body>
+      <h1>Home</h1>
+    </body>
+  )
+}
