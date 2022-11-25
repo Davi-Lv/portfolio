9403118a66d3cc82adae5e4889f2ed02a1f29b1b
Davi-Lv <josedavimordonhoguimaraes@gmail.com>
2022-11-25 00:59:32 -0300

    portfolio atualizado. back-end quase finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import './App.css'
 
+const nomeDoProjeto = ["Calculadora com Java e React.js", "Sei lá"]
+
+const ListaNomeDoProjeto = nomeDoProjeto.map(
+  (n, i) => {
+    <p key={i}>{n}</p>
+  }
+)
+
 export default function App() {
   return (
     <body className="App">
@@ -18,7 +26,7 @@ export default function App() {
         </div>
 
         <div className='botaoContatos'>
-          Contatos
+          Meus contatos
         </div>
       </header>
 
@@ -27,7 +35,7 @@ export default function App() {
           <h1 className='olaeusou'>Olá, eu sou</h1>
           <h1 className='davimordonho'>Davi Mordonho</h1>
           <p className='paragrafoQuemSou'>Estudante de analise e desenvolvimento de sistemas no Instituto Federal de Pernambuco</p>
-          <button className="saberMais"><span>Saber mais</span></button>
+          <button className="saberMais"><span>Contatos</span></button>
         </div>
 
         <div className='imgQuemSou'>
@@ -38,31 +46,31 @@ export default function App() {
       <div className="habilidades">
 
         <h1>Habilidade</h1>
-        <div className="bloco">
-          <div>
+        <div className="bloco1">
+          <div className='iconAndName'>
             <img src="../src/assets/html5.svg" alt="HTML 5" />
             <p>HTML 5</p>
           </div>
-          <div>
+          <div className='iconAndName'>
             <img src="../src/assets/css3.svg" alt="CSS 3" />
             <p>CSS 3</p>
           </div>
-          <div>
+          <div className='iconAndName'>
             <img src="../src/assets/java.svg" alt="Java" />
             <p>Java</p>
           </div>
         </div>
-        <div className="bloco">
-          <div>
+        <div className="bloco2">
+          <div className='iconAndName'>
             <img src="../src/assets/javascript.svg" alt="JavaScript" />
             <p>JavaScript</p>
             <p></p>
           </div>
-          <div>
+          <div className='iconAndName'>
             <img src="../src/assets/react.svg" alt="React e React Native" />
             <p>React.Js</p>
           </div>
-          <div>
+          <div className='iconAndName'>
             <img src="../src/assets/gitEGithub.svg" alt="Git e GitHub" />
             <p>Git e GitHub</p>
           </div>
@@ -70,21 +78,32 @@ export default function App() {
 
         <div className="linha" />
 
-        <div className="bloco">
-          <div>
+        <div className="bloco3">
+          <div className='iconAndName'>
             <img src="../src/assets/figma.svg" alt="Figma" />
             <p>Figma</p>
           </div>
-          <div>
+          <div className='iconAndName'>
             <img src="../src/assets/photoshop.svg" alt="Photoshop" />
             <p>Photoshop</p>
           </div>
-          <div>
+          <div className='iconAndName'>
             <img src="../src/assets/ilustrator.svg" alt="Ilustrator" />
             <p>Ilustrator</p>
           </div>
         </div >
       </div >
+
+      <div className="meusProjetos">
+        <h1>Meus Projetos</h1>
+
+        <div className="BlocoProjeto">
+          <div className="picture">
+            <img src="../src/assets/exemploImagemProjeto.png" alt="Imagem do Projeto" />
+          </div>
+          <p>{ListaNomeDoProjeto[1]}</p>
+        </div>
+      </div>
 
     </body >
   )
